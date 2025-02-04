d7da05b1e0d495aee3b113f4f17728dbe16f534f
Aditya Gautam <111475655+adityabbdumca@users.noreply.github.com>
2025-02-04 09:19:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['5642hy-5174.csb.app'] // Allowed host added here
+  }
 })
