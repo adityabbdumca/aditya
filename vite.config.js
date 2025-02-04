b84c8584fe0800684f93d6d54784bfda4cae7e99
Aditya Gautam <111475655+adityabbdumca@users.noreply.github.com>
2025-02-04 10:19:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['5642hy-5174.csb.app'] // Allowed host added here
+    host: '0.0.0.0', // Required for Codesandbox
+    allowedHosts: [
+      '.csb.app' // Allow ALL *.csb.app subdomains
+    ]
   }
 })
